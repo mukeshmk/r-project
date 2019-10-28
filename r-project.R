e752b279b2089192231a10a0c890d1504beac5d4
Mukesh A <amukesh.mk@gmail.com>
2019-10-28 18:44:24 +0000

    partitioning the data into training and test data

--- r-project.R
+++ r-project.R
@@ -15,10 +15,18 @@ attach(proj_data)
 # structure of the data_set
 str(proj_data)
 
+# setting the random seed so that partition is reproducible
+set.seed(456)
+train_ind <- sample(seq_len(nrow(proj_data)), size = floor(0.8 * nrow(proj_data)))
+
+# Spliting the Data Set into Training and Testing data
+train_data <- data.frame(proj_data[train_ind, ])
+test_data <- data.frame(proj_data[-train_ind, ])
+
 # creating a model where all the data (.) is used and 'Response' is the the Target variable (~)
 # method='class' is used for a classification tree
 # model includes all the X's, Y's and Group
-proj_model_resp <-rpart(Response ~ ., data=proj_data, method='class', 
+proj_model_resp <-rpart(Response ~ ., data=train_data, method='class', 
                         control=rpart.control(minsplit=30, minbucket=15, maxdepth=8 ))
 # ploting the DT with all the data
 plot(as.party(proj_model_resp))
@@ -26,38 +34,38 @@ plot(as.party(proj_model_resp))
 # converting the 'Response' into a categorical value 'target'
 target = ifelse(Response==1,'Y','N')
 
-proj_data <- data.frame(proj_data, target)
+train_data <- data.frame(train_data, target)
 # removing 'Response' from the data
-proj_data <- proj_data[,-1]
+train_data <- train_data[,-1]
 
 # creating the same model with the Target variable as the categorical value
 # model includes all the X's, Y's and Group
-proj_model_tar <-rpart(target ~ ., data=proj_data, method='class', 
+proj_model_tar <-rpart(target ~ ., data=train_data, method='class', 
                        control=rpart.control(minsplit=30, minbucket=15, maxdepth=8 ))
 plot(as.party(proj_model_tar))
 
 # getting only X cols
-Xdata = proj_data[c(2:8)]
+Xdata = train_data[c(2:8)]
 # creating a model with only X's
 proj_model_X <-rpart(target ~ ., data=Xdata, method='class', 
                      control=rpart.control(minsplit=30, minbucket=15, maxdepth=8 ))
 plot(as.party(proj_model_X))
 
 # getting only Y cols
-Ydata = proj_data[c(9:15)]
+Ydata = train_data[c(9:15)]
 # creating a model with only Y's
 proj_model_Y <-rpart(target ~ ., data=Ydata, method='class', 
                      control=rpart.control(minsplit=30, minbucket=15, maxdepth=8 ))
 plot(as.party(proj_model_Y))
 
 # creating a model with X's and Y's without Group
-XYdata = proj_data[c(2:14)]
+XYdata = train_data[c(2:14)]
 proj_model_XY <-rpart(target ~ ., data=XYdata, method='class', 
                       control=rpart.control(minsplit=30, minbucket=15, maxdepth=8 ))
 plot(as.party(proj_model_XY))
 
 # getting data of only X's and Group
-XGdata = proj_data[c(1:8, 16)]
+XGdata = train_data[c(1:8, 16)]
 # spliting data based on Group
 XGdata = split(XGdata, XGdata$Group)
 XGdata0 = XGdata[[1]]
@@ -74,7 +82,7 @@ proj_model_XG1 <-rpart(XGdata1$target ~ ., data=XGdata1, method='class',
 plot(as.party(proj_model_XG1))
 
 # getting data of only Y's and Group
-YGdata = proj_data[c(1, 9:15, 16)]
+YGdata = train_data[c(1, 9:15, 16)]
 # spliting data based on Group
 YGdata = split(YGdata, YGdata$Group)
 YGdata0 = YGdata[[1]]
@@ -91,7 +99,7 @@ proj_model_YG1 <-rpart(YGdata1$target ~ ., data=YGdata1, method='class',
 plot(as.party(proj_model_YG1))
 
 # spliting data based on Group
-XYGdata = split(proj_data, proj_data$Group)
+XYGdata = split(train_data, train_data$Group)
 XYGdata0 = XYGdata[[1]]
 XYGdata1 = XYGdata[[2]]
 # creating a model with XY's and Group 0
